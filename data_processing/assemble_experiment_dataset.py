797209518772f6f2cc4f6e5cc0a0940b4a30eb95
Clément Dauvilliers <clement.dauvilliers@inria.fr>
2024-03-08 18:13:10 +0100

    Cfg-taken num_workers

--- data_processing/assemble_experiment_dataset.py
+++ data_processing/assemble_experiment_dataset.py
@@ -1,6 +1,7 @@
 """
 Implements a function to assemble the dataset for the experiment.
 """
+
 import torch
 from data_processing.custom_dataset import SuccessiveStepsDataset
 from data_processing.datasets import load_tcir
@@ -8,7 +9,6 @@ from utils.sampling import inverse_intensity_sampler
 from utils.datacube import datacube_to_tensor
 from utils.train_test_split import train_val_test_split
 from utils.utils import hours_to_sincos
-
 
 
 def load_dataset(cfg, input_variables, tabular_tasks, datacube_tasks):
@@ -40,23 +40,22 @@ def load_dataset(cfg, input_variables, tabular_tasks, datacube_tasks):
     # ====== LOAD DATASET ====== #
     # Load the TCIR dataset
     tcir_info, tcir_datacube = load_tcir()
-    print('TCIR dataset loaded')
-    print('Memory usage: {:.2f} GB'.format(tcir_datacube.nbytes / 1e9))
+    print("TCIR dataset loaded")
+    print("Memory usage: {:.2f} GB".format(tcir_datacube.nbytes / 1e9))
 
     # Add a column with the sin/cos encoding of the hours, which will be used as input
     # to the model
-    sincos_hours = hours_to_sincos(tcir_info['ISO_TIME'])
-    tcir_info['HOUR_SIN'], tcir_info['HOUR_COS'] = sincos_hours[:, 0], sincos_hours[:, 1] 
+    sincos_hours = hours_to_sincos(tcir_info["ISO_TIME"])
+    tcir_info["HOUR_SIN"], tcir_info["HOUR_COS"] = sincos_hours[:, 0], sincos_hours[:, 1]
 
     # Convert the datacube to a tensor
     tcir_datacube = datacube_to_tensor(tcir_datacube)
 
     # ====== TRAIN/VAL/TEST SPLIT ====== #
     # Split the dataset into train, validation and test sets
-    train_index, val_index, test_index = train_val_test_split(tcir_info,
-                                                              train_size=0.6,
-                                                              val_size=0.2,
-                                                              test_size=0.2)
+    train_index, val_index, test_index = train_val_test_split(
+        tcir_info, train_size=0.6, val_size=0.2, test_size=0.2
+    )
     # Trajectory
     train_trajs = tcir_info.iloc[train_index]
     val_trajs = tcir_info.iloc[val_index]
@@ -72,16 +71,29 @@ def load_dataset(cfg, input_variables, tabular_tasks, datacube_tasks):
     # ====== DATASET CREATION ====== #
     # Data augmentation is only applied to the training dataset if requested
     apply_data_aug = False
-    if 'data_augmentation' in cfg['training_settings']:
-        apply_data_aug = cfg['training_settings']['data_augmentation']
+    if "data_augmentation" in cfg["training_settings"]:
+        apply_data_aug = cfg["training_settings"]["data_augmentation"]
     # Create the train and validation datasets.
-    train_dataset = SuccessiveStepsDataset(train_trajs, input_variables, tabular_tasks,
-                                           {'tcir': train_patches}, ['tcir'], datacube_tasks,
-                                           cfg,
-                                           random_rotations=apply_data_aug)
-    val_dataset = SuccessiveStepsDataset(val_trajs, input_variables, tabular_tasks,
-                                         {'tcir': val_patches}, ['tcir'], datacube_tasks,
-                                         cfg, random_rotations=False)
+    train_dataset = SuccessiveStepsDataset(
+        train_trajs,
+        input_variables,
+        tabular_tasks,
+        {"tcir": train_patches},
+        ["tcir"],
+        datacube_tasks,
+        cfg,
+        random_rotations=apply_data_aug,
+    )
+    val_dataset = SuccessiveStepsDataset(
+        val_trajs,
+        input_variables,
+        tabular_tasks,
+        {"tcir": val_patches},
+        ["tcir"],
+        datacube_tasks,
+        cfg,
+        random_rotations=False,
+    )
     # Normalize the data. For the validation dataset, we use the mean and std of the training dataset.
     # The normalization constants are saved in the tabular_tasks dictionary.
     train_dataset.normalize_inputs()
@@ -89,14 +101,24 @@ def load_dataset(cfg, input_variables, tabular_tasks, datacube_tasks):
     val_dataset.normalize_inputs(other_dataset=train_dataset)
     val_dataset.normalize_outputs(other_dataset=train_dataset)
     # Create the sampler and data loaders
-    batch_size = cfg['training_settings']['batch_size']
-    if ('sampling_weights' in cfg['training_settings']) and (cfg['training_settings']['sampling_weights']):
+    batch_size = cfg["training_settings"]["batch_size"]
+    num_workers = (
+        cfg["training_settings"]["num_workers"] if "num_workers" in cfg["training_settings"] else 0
+    )
+    if ("sampling_weights" in cfg["training_settings"]) and (
+        cfg["training_settings"]["sampling_weights"]
+    ):
         # Create the sampler
-        sampler = inverse_intensity_sampler(train_dataset.get_sample_intensities(),
-                                            plot_weights='figures/weights.png')
-        train_loader = torch.utils.data.DataLoader(train_dataset, sampler=sampler, batch_size=batch_size)
+        sampler = inverse_intensity_sampler(
+            train_dataset.get_sample_intensities(), plot_weights="figures/weights.png"
+        )
+        train_loader = torch.utils.data.DataLoader(
+            train_dataset, sampler=sampler, batch_size=batch_size, num_workers=num_workers
+        )
     else:
-        train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+        train_loader = torch.utils.data.DataLoader(
+            train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers
+        )
     val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
-    
+
     return train_dataset, val_dataset, train_loader, val_loader
