b3167675a8958d82bb5da589ba90f5a3035318e3
Clément Dauvilliers <clement.dauvilliers@inria.fr>
2024-03-26 14:09:21 +0100

    Added -g option

--- utils/wandb.py
+++ utils/wandb.py
@@ -6,15 +6,18 @@ import wandb
 from experiments.training import create_tasks
 
 
-def retrieve_wandb_runs(run_ids):
+def retrieve_wandb_runs(run_ids=None, group=None):
     """
     For a set of W&B run ids, retrieve the corresponding runs, and
     returns their configurations.
 
     Parameters
     ----------
-    run_ids : list of str
+    run_ids : list of str, optional
         The ids of the runs to retrieve.
+    group: str, optional
+        The group of runs to retrieve. Exactly one of `run_ids` and `group`
+        must be provided.
 
     Returns
     -------
@@ -22,23 +25,32 @@ def retrieve_wandb_runs(run_ids):
     configs: Mapping run_id -> config
     tasks: Mapping run_id -> Mapping task_name -> task dict
     """
+    if run_ids is None and group is None:
+        raise ValueError("Either `run_ids` or `group` must be provided.")
+    if run_ids is not None and group is not None:
+        raise ValueError("Only one of `run_ids` and `group` should be provided.")
     api = wandb.Api()
-    configs, runs, tasks = {}, {}, {}
+    # If a group is provided, retrieve the runs in the group
+    if group is not None:
+        runs = api.runs("arches/tc_prediction", filters={"group": group})
+        run_ids = [run.id for run in runs]
+    # Otherwise, retrieve the runs with the provided ids
+    else:
+        runs = {}
+        for run_id in run_ids:
+            runs[run_id] = api.run(f"arches/tc_prediction/{run_id}")
+    # Retrieve the configurations
+    configs, tasks = {}, {}
     for run_id in run_ids:
-        try:
-            run = api.run(f"arches/tc_prediction/{run_id}")
-        except wandb.errors.CommError:
-            print(f"WARNING: Run {run_id} could not be retrieved.")
-        runs[run_id] = run
-        configs[run_id] = run.config
+        configs[run_id] = runs[run_id].config
 
         # Backward compatibility: replace the "future_steps" key by
         # "target_steps":
-        exp_cfg = configs[run_id]['experiment']
-        if 'future_steps' in exp_cfg:
-            exp_cfg['target_steps'] = [t for t in range(1, exp_cfg['future_steps'] + 1)]
-            if 'perform_estimation' in exp_cfg:
-                exp_cfg['target_steps'] += [-t for t in range(0, exp_cfg['past_steps'])]
+        exp_cfg = configs[run_id]["experiment"]
+        if "future_steps" in exp_cfg:
+            exp_cfg["target_steps"] = [t for t in range(1, exp_cfg["future_steps"] + 1)]
+            if "perform_estimation" in exp_cfg:
+                exp_cfg["target_steps"] += [-t for t in range(0, exp_cfg["past_steps"])]
 
         # Retrieve the tasks performed by the run
         tasks[run_id] = create_tasks(configs[run_id])
