36168986ad79aee918a923eaff76ba407d415658
Clément Dauvilliers <clem.dauvilliers@gmail.com>
2024-04-10 00:28:27 +0200

    Intermediate saving

--- scripts/preprocess_tcir.py
+++ scripts/preprocess_tcir.py
@@ -24,176 +24,214 @@ if __name__ == "__main__":
     # Load config file
     with open("config.yml", "r") as cfg_file:
         cfg = yaml.safe_load(cfg_file)
-    # Load the paths to the two parts of the dataset
+    # Paths to the TCIR dataset
     _TCIR_PATH_1_ = cfg["paths"]["tcir_atln"]  # Atlantic part
     _TCIR_PATH_2_ = cfg["paths"]["tcir_sh"]  # Southern Hemisphere part
+    # Path to the ERA5 patches
+    _ERA5_PATCHES_PATH_ = cfg["paths"]["era5_patches"]
+    # Output directory
+    save_dir = Path(cfg["paths"]["tcir_preprocessed_dir"])
+    interm_cube_path = save_dir / "datacube_intermediate.nc"
+    interm_info_path = save_dir / "info_intermediate.csv"
 
-    print("Loading the raw TCIR dataset...")
-    # Load the tabular information
-    data_info = pd.concat(
-        [
-            pd.read_hdf(_TCIR_PATH_1_, key="info", mode="r"),
-            pd.read_hdf(_TCIR_PATH_2_, key="info", mode="r"),
-        ]
-    )
-
-    # Load the datacubes and concatenate them at once
-    datacube = xr.combine_nested(
-        [xr.open_dataset(_TCIR_PATH_1_)["matrix"], xr.open_dataset(_TCIR_PATH_2_)["matrix"]],
-        concat_dim="phony_dim_4",
-    )
-
-    # === TABULAR DATA PREPROCESSING ===
-    # Rename the columns to match IBTrACS
-    data_info = data_info.rename(
-        {
-            "data_set": "BASIN",
-            "ID": "SID",
-            "time": "ISO_TIME",
-            "lat": "LAT",
-            "lon": "LON",
-            "Vmax": "INTENSITY",
-        },
-        axis="columns",
-    )
-    # Convert ISO_TIME to datetime
-    data_info["ISO_TIME"] = pd.to_datetime(data_info["ISO_TIME"], format="%Y%m%d%H")
-
-    # === OPTIONAL SUBSAMPLING ===
-    # If the dataset is too large, we can subsample it by selecting a random subset of the
-    # storms. This is done by specifying the 'tcir/subsample' key in the config file.
-    if cfg["preprocessing"]["subsample"]:
-        fraction = cfg["preprocessing"]["subsample_fraction"]
-        # Select a random subset of the storms
-        sids = data_info["SID"].unique()
-        sids = np.random.default_rng(42).choice(
-            sids, size=int(len(sids) * fraction), replace=False
+    # Check if the intermediate data is already saved
+    if not (interm_cube_path.exists() and interm_info_path.exists()):
+        print("Loading the raw TCIR dataset...")
+        # Load the tabular information
+        data_info = pd.concat(
+            [
+                pd.read_hdf(_TCIR_PATH_1_, key="info", mode="r"),
+                pd.read_hdf(_TCIR_PATH_2_, key="info", mode="r"),
+            ]
         )
-        data_info = data_info[data_info["SID"].isin(sids)]
+
+        # Load the datacubes and concatenate them at once
+        datacube = xr.combine_nested(
+            [xr.open_dataset(_TCIR_PATH_1_)["matrix"], xr.open_dataset(_TCIR_PATH_2_)["matrix"]],
+            concat_dim="phony_dim_4",
+        )
+        # Chunk the datacube
+        datacube = datacube.chunk({"phony_dim_4": 500})
+
+        # === TABULAR DATA PREPROCESSING ===
+        # Rename the columns to match IBTrACS
+        data_info = data_info.rename(
+            {
+                "data_set": "BASIN",
+                "ID": "SID",
+                "time": "ISO_TIME",
+                "lat": "LAT",
+                "lon": "LON",
+                "Vmax": "INTENSITY",
+            },
+            axis="columns",
+        )
+        # Convert ISO_TIME to datetime
+        data_info["ISO_TIME"] = pd.to_datetime(data_info["ISO_TIME"], format="%Y%m%d%H")
+
+        # === OPTIONAL SUBSAMPLING ===
+        # If the dataset is too large, we can subsample it by selecting a random subset of the
+        # storms. This is done by specifying the 'tcir/subsample' key in the config file.
+        if cfg["preprocessing"]["subsample"]:
+            fraction = cfg["preprocessing"]["subsample_fraction"]
+            # Select a random subset of the storms
+            sids = data_info["SID"].unique()
+            sids = np.random.default_rng(42).choice(
+                sids, size=int(len(sids) * fraction), replace=False
+            )
+            data_info = data_info[data_info["SID"].isin(sids)]
+            # Select the corresponding entries in the datacube
+            datacube = datacube.isel(phony_dim_4=data_info.index)
+            # Reset the index of data_info so that it matches datacube.isel
+            data_info = data_info.reset_index(drop=True)
+            print(f"Subsampled to {len(data_info['SID'].unique())} storms.")
+
+        # === TEMPORAL SELECTION ===
+        # The temporal resolution in TCIR is 3 hours, but the best-track data's original resolution
+        # (such as ATCF given by the JTWC) is 6 hours. One point out of two was linearly interpolated
+        # (which is very common with best-track data), which would make the problem artificially easier.
+        # To avoid this, we'll only keep the points that are multiples of 6 hours.
+        # Select the points in the info
+        data_info = data_info[data_info["ISO_TIME"].dt.hour.isin([0, 6, 12, 18])]
         # Select the corresponding entries in the datacube
         datacube = datacube.isel(phony_dim_4=data_info.index)
         # Reset the index of data_info so that it matches datacube.isel
         data_info = data_info.reset_index(drop=True)
-        print(f"Subsampled to {len(data_info['SID'].unique())} storms.")
 
-    # === TEMPORAL SELECTION ===
-    # The temporal resolution in TCIR is 3 hours, but the best-track data's original resolution
-    # (such as ATCF given by the JTWC) is 6 hours. One point out of two was linearly interpolated
-    # (which is very common with best-track data), which would make the problem artificially easier.
-    # To avoid this, we'll only keep the points that are multiples of 6 hours.
-    # Select the points in the info
-    data_info = data_info[data_info["ISO_TIME"].dt.hour.isin([0, 6, 12, 18])]
-    # Select the corresponding entries in the datacube
-    datacube = datacube.isel(phony_dim_4=data_info.index)
-    # Reset the index of data_info so that it matches datacube.isel
-    data_info = data_info.reset_index(drop=True)
+        # === DATACUBE PREPROCESSING ===
+        # Rename the dimensions
+        datacube = datacube.rename(
+            {
+                "phony_dim_4": "sid_time",
+                "phony_dim_5": "v_pixel_offset",
+                "phony_dim_6": "h_pixel_offset",
+                "phony_dim_7": "variable",
+            }
+        )
+        # Remove the Water vapor and Visible channels, as supposedly do not
+        # provide information that complements the IR channel.
+        # Besides, the Visible channel is unstable due to daylight.
+        datacube = datacube.drop_sel(variable=[1, 2])
 
-    # === DATACUBE PREPROCESSING ===
-    # Rename the dimensions
-    datacube = datacube.rename(
-        {
-            "phony_dim_4": "sid_time",
-            "phony_dim_5": "v_pixel_offset",
-            "phony_dim_6": "h_pixel_offset",
-            "phony_dim_7": "variable",
-        }
-    )
-    # Remove the Water vapor and Visible channels, as supposedly do not
-    # provide information that complements the IR channel.
-    # Besides, the Visible channel is unstable due to daylight.
-    datacube = datacube.drop_sel(variable=[1, 2])
+        # Add coordinates for the latitude and longitude dimensions
+        # Since the lat/lon differs for each storm and time, we'll use the offset
+        # (in pixels) from the center of the image
+        img_side_len = datacube.shape[1]
+        offset = np.arange(-(img_side_len // 2), img_side_len // 2 + 1)
+        datacube = datacube.assign_coords(
+            h_pixel_offset=("h_pixel_offset", offset), v_pixel_offset=("v_pixel_offset", offset)
+        )
 
-    # Add coordinates for the latitude and longitude dimensions
-    # Since the lat/lon differs for each storm and time, we'll use the offset
-    # (in pixels) from the center of the image
-    img_side_len = datacube.shape[1]
-    offset = np.arange(-(img_side_len // 2), img_side_len // 2 + 1)
-    datacube = datacube.assign_coords(
-        h_pixel_offset=("h_pixel_offset", offset), v_pixel_offset=("v_pixel_offset", offset)
-    )
+        # Add coordinates for the sid_time dimension
+        # This coordinate will be in the format of a tuple (SID, ISO_TIME)
+        datacube = datacube.assign_coords(
+            sid=("sid_time", data_info["SID"].values), time=("sid_time", data_info["ISO_TIME"].values)
+        )
 
-    # Add coordinates for the sid_time dimension
-    # This coordinate will be in the format of a tuple (SID, ISO_TIME)
-    datacube = datacube.assign_coords(
-        sid=("sid_time", data_info["SID"].values), time=("sid_time", data_info["ISO_TIME"].values)
-    )
+        # Finally, we'll add a coordinate for the variable dimension
+        datacube = datacube.assign_coords(variable=("variable", ["IR", "PMW"]))
 
-    # Finally, we'll add a coordinate for the variable dimension
-    datacube = datacube.assign_coords(variable=("variable", ["IR", "PMW"]))
+        # Plot the histogram of the pixel values for each channel
+        # Set the y-axis to log scale
+        fig, axes = plt.subplots(1, 2, figsize=(12, 6))
+        for k, var in enumerate(["IR", "PMW"]):
+            datacube.sel(variable=var).plot.hist(ax=axes[k], bins=100)
+            axes[k].set_title(f"{var} channel")
+            axes[k].set_yscale("log")
+        plt.savefig("figures/pixel_histograms.png")
 
-    # Plot the histogram of the pixel values for each channel
-    # Set the y-axis to log scale
-    fig, axes = plt.subplots(1, 2, figsize=(12, 6))
-    for k, var in enumerate(["IR", "PMW"]):
-        datacube.sel(variable=var).plot.hist(ax=axes[k], bins=100)
-        axes[k].set_title(f"{var} channel")
-        axes[k].set_yscale("log")
-    plt.savefig("figures/pixel_histograms.png")
+        # === OUTLIERS AND MISSING VALUES ==== (SEE NOTEBOOK FOR EXPLAINATIONS)
+        print("Processing outliers and missing values...")
+        # Convert the unreasonably large values to NaNs
+        datacube = datacube.where(datacube[:, :, :, 1] < 10 ^ 3)
+        # Compute the ratio of NaNs (native + converted from outliers) for each sample
+        nan_counts = np.array([datacube[k].isnull().sum() for k in range(datacube.shape[0])])
+        nan_ratios = nan_counts / (datacube.shape[1] * datacube.shape[2] * 2)
+        print("Average NaN ratio: ", nan_ratios.mean(), " std: ", nan_ratios.std())
 
-    # === OUTLIERS AND MISSING VALUES ==== (SEE NOTEBOOK FOR EXPLAINATIONS)
-    print("Processing outliers and missing values...")
-    # Convert the unreasonably large values to NaNs
-    datacube = datacube.where(datacube[:, :, :, 1] < 10 ^ 3)
-    # Compute the ratio of NaNs (native + converted from outliers) for each sample
-    nan_counts = np.array([datacube[k].isnull().sum() for k in range(datacube.shape[0])])
-    nan_ratios = nan_counts / (datacube.shape[1] * datacube.shape[2] * 2)
-    print("Average NaN ratio: ", nan_ratios.mean(), " std: ", nan_ratios.std())
+        # * For every storm S that contains at least one sample that is more than 10% NaN:
+        #   * Find all segments of S's trajectory that do not contain any NaN;
+        #   * Consider each of those segments as an independent trajectory, by giving them new SIDs.
+        #   * Discard the samples containing more than 10% of NaNs.
+        # * Where the images contain less than 10% of NaNs, fill them with zeros.
 
-    # * For every storm S that contains at least one sample that is more than 10% NaN:
-    #   * Find all segments of S's trajectory that do not contain any NaN;
-    #   * Consider each of those segments as an independent trajectory, by giving them new SIDs.
-    #   * Discard the samples containing more than 10% of NaNs.
-    # * Where the images contain less than 10% of NaNs, fill them with zeros.
+        # Retrieve the index of the samples that are fully NaN (in the sense of over 10%)
+        where_full_nan = np.where(nan_ratios >= 0.1)[0]
+        # Retrieve the SIDs corresponding to those samples
+        sids_with_full_nan = data_info.iloc[where_full_nan]["SID"].unique()
+        print('"Full" NaN proportion: ', where_full_nan.shape[0] / datacube.shape[0])
+        print(
+            'Proportion of storms that include at least one "full" NaN sample: ',
+            sids_with_full_nan.shape[0] / data_info["SID"].unique().shape[0],
+        )
 
-    # Retrieve the index of the samples that are fully NaN (in the sense of over 10%)
-    where_full_nan = np.where(nan_ratios >= 0.1)[0]
-    # Retrieve the SIDs corresponding to those samples
-    sids_with_full_nan = data_info.iloc[where_full_nan]["SID"].unique()
-    print('"Full" NaN proportion: ', where_full_nan.shape[0] / datacube.shape[0])
-    print(
-        'Proportion of storms that include at least one "full" NaN sample: ',
-        sids_with_full_nan.shape[0] / data_info["SID"].unique().shape[0],
-    )
+        print("Processing storms including at least one full-NaN sample...")
+        for sid in tqdm(sids_with_full_nan):
+            segments = []
+            current_segment = []
+            currently_in_segment = False
+            # Iterate through the samples of that storm. If we find a NaN, that's the end
+            # of the current segment. Keep iterating until finding a non full-NaN sample,
+            # which is the start of a new segment.
+            for k in data_info[data_info["SID"] == sid].index:
+                if k in where_full_nan:
+                    if currently_in_segment:
+                        # Stop the current segment
+                        segments.append(np.array(current_segment))
+                        current_segment = []
+                        currently_in_segment = False
+                else:
+                    if not currently_in_segment:
+                        # Start a new segment if not currently in a segment
+                        currently_in_segment = True
+                    current_segment.append(k)
+            # Add the last segment
+            if currently_in_segment:
+                segments.append(np.array(current_segment))
+            # Give all samples in that segment a new SID
+            sid_values = data_info["SID"].values.copy()
+            for n_seg, seg in enumerate(segments):
+                sid_values[seg] = sid_values[seg] + f"_{n_seg}"
+            data_info["SID"] = sid_values
+        # Drop the samples that are fully NaNs
+        data_info = data_info.drop(index=where_full_nan)
+        # Select the index of data_info (which doesn't contain the full-NaN samples anymore) in the datacube
+        datacube = datacube.isel(sid_time=data_info.index)
+        # Reset the index of the tabular data, so that it matches that of the datacube
+        data_info = data_info.reset_index(drop=True)
+        # We can now interpolate the partially-NaN images
+        datacube = datacube.ffill("h_pixel_offset")
+        datacube = datacube.ffill("v_pixel_offset")
+        # NaN values at the border won't be interpolated, we'll fill them with zeros.
+        datacube = datacube.fillna(0)
 
-    print("Processing storms including at least one full-NaN sample...")
-    for sid in tqdm(sids_with_full_nan):
-        segments = []
-        current_segment = []
-        currently_in_segment = False
-        # Iterate through the samples of that storm. If we find a NaN, that's the end
-        # of the current segment. Keep iterating until finding a non full-NaN sample,
-        # which is the start of a new segment.
-        for k in data_info[data_info["SID"] == sid].index:
-            if k in where_full_nan:
-                if currently_in_segment:
-                    # Stop the current segment
-                    segments.append(np.array(current_segment))
-                    current_segment = []
-                    currently_in_segment = False
-            else:
-                if not currently_in_segment:
-                    # Start a new segment if not currently in a segment
-                    currently_in_segment = True
-                current_segment.append(k)
-        # Add the last segment
-        if currently_in_segment:
-            segments.append(np.array(current_segment))
-        # Give all samples in that segment a new SID
-        sid_values = data_info["SID"].values.copy()
-        for n_seg, seg in enumerate(segments):
-            sid_values[seg] = sid_values[seg] + f"_{n_seg}"
-        data_info["SID"] = sid_values
-    # Drop the samples that are fully NaNs
-    data_info = data_info.drop(index=where_full_nan)
-    # Select the index of data_info (which doesn't contain the full-NaN samples anymore) in the datacube
-    datacube = datacube.isel(sid_time=data_info.index)
-    # Reset the index of the tabular data, so that it matches that of the datacube
-    data_info = data_info.reset_index(drop=True)
-    # We can now interpolate the partially-NaN images
-    datacube = datacube.ffill("h_pixel_offset")
-    datacube = datacube.ffill("v_pixel_offset")
-    # NaN values at the border won't be interpolated, we'll fill them with zeros.
-    datacube = datacube.fillna(0)
+        # Save the intermediate datacube
+        print("Saving the intermediate datacube and info...")
+        datacube.to_netcdf(interm_cube_path, compute=True)
+        # Save the intermediate info
+        data_info.to_csv(interm_info_path, index=True)
+    else:
+        # Load the intermediate data
+        print("Loading the intermediate data from ", save_dir)
+        datacube = xr.open_dataarray(save_dir / "datacube_intermediate.nc",
+                                     chunks={"sid_time": 500})
+        data_info = pd.read_csv(save_dir / "info_intermediate.csv", index_col=0)
+
+    # === CONCATENATION WITH ERA5 =======
+    # Load the ERA5 patches
+    era5 = xr.open_mfdataset(_ERA5_PATCHES_PATH_ + "/*_surf*.nc", combine="nested", concat_dim="sid_time",
+                             chunks={"sid_time": 500})
+    # Add a 'sid_time' index to ERA5 and to TCIR
+    era5 = era5.set_xindex(['sid', 'time'])
+    datacube = datacube.set_xindex(['sid', 'time'])
+    # Convert the TCIR DataArray to a Dataset so that we can use merge():
+    datacube = datacube.to_dataset(dim="variable")
+    # Merge the ERA5 dataset with the TCIR dataset
+    datacube = xr.merge([datacube, era5], compat="equals", join="inner")
+    # Convert back to a DataArray
+    datacube = datacube.to_array(dim="variable")
+    # Destroy the MultiIndex, which won't be needed anymore and can't be saved to netCDF
+    datacube = datacube.reset_index("sid_time")
 
     # === FEATURE ENGINEERING ===
     # Add the time of the day as a feature
