4b64b33303a495a2de70f8c58fdb59174706b3af
Clément Dauvilliers <clement.dauvilliers@inria.fr>
2024-04-12 17:59:14 +0200

    Displayed names and countplot

--- experiments/eval_crossval.py
+++ experiments/eval_crossval.py
@@ -27,7 +27,8 @@ if __name__ == "__main__":
         "-g",
         "--groups",
         nargs="+",
-        help="W&B groups to evaluate."
+        help="W&B groups to evaluate.\
+            Cannot be used with --ids.",
     )
     parser.add_argument(
         "-i",
@@ -40,18 +41,29 @@ if __name__ == "__main__":
         "-m",
         "--metric",
         type=str,
-        default="CRPS",
-        help="Name of the metric to evaluate. Default is CRPS.",
+        required=True,
+        help="Name of the metric to evaluate.",
     )
     parser.add_argument(
-            "-t",
-            "--task",
-            type=str,
-            default="vmax",
-            help="Name of the task to evaluate. Default is vmax.",
+        "-t",
+        "--task",
+        type=str,
+        default="vmax",
+        help="Name of the task to evaluate. Default is vmax.",
+    )
+    parser.add_argument(
+        "-d",
+        "--display",
+        type=str,
+        nargs="+",
+        default=[],
+        help="List of names to display instead of the W&B run names.\
+                Must have the same length and order as --ids or --groups.",
     )
     args = parser.parse_args()
     if args.groups is not None:
+        if args.ids is not None:
+            raise ValueError("Cannot provide both --ids and --groups.")
         args.ids = None
         groups = args.groups
     else:
@@ -77,6 +89,21 @@ if __name__ == "__main__":
     run_ids = list(runs.keys())
     # Retrieve the run name for each run id
     run_names = {run_id: configs[run_id]["experiment"]["name"] for run_id in run_ids}
+
+    # Retrieve the display names if provided
+    if len(args.display) > 0:
+        # If --ids was provided, use the run ids as keys
+        if args.ids is not None:
+            display_names = {run_id: name for run_id, name in zip(args.ids, args.display)}
+        # If --groups was provided, use the group names as keys
+        elif args.groups is not None:
+            display_names = {group: name for group, name in zip(args.groups, args.display)}
+    # Otherwise, use the run names or group names depending on what's available
+    else:
+        if args.ids is not None:
+            display_names = run_names
+        elif args.groups is not None:
+            display_names = {group: group for group in groups}
 
     # =================== DATA LOADING =================== #
     # Load the predictions and targets for every run
@@ -133,6 +160,12 @@ if __name__ == "__main__":
             metric: col_crps,
         }
     )
+    # If --groups was provided, add a column with the display names of the groups
+    if args.groups is not None:
+        results["display_name"] = results["group"].apply(lambda x: display_names[x])
+    # If --ids was provided, add a column with the display names of the runs
+    if args.ids is not None:
+        results["display_name"] = results["run_id"].apply(lambda x: display_names[x])
     # Compute the mean and the std of the metric for each group
     # to obtain a DF (group, mean_metric, std_metric)
     group_results = results.groupby("group").agg({metric: ["mean", "std"]})
@@ -141,20 +174,24 @@ if __name__ == "__main__":
     group_results = group_results.reset_index()
     # Save the results to a CSV file
     results.to_csv(results_dir / "results.csv", index=False)
-    
+
     # ================== OPTIONAL AGGREGATION =================== #
     # If the metric requires an aggregation step, apply it now
     # We'll create two now DFs: one with the results aggregated by time step
     # and another one with the results aggregated by SSHS category
     if original_metric == "RMSE":
         results_per_time = results.rename(columns={"MSE": "RMSE"})
-        results_per_time = results_per_time[['group', 'time_step', 'RMSE']]
-        results_per_time = results_per_time.groupby(["group", "time_step"])['RMSE'].mean().reset_index()
-        results_per_time['RMSE'] = np.sqrt(results_per_time['RMSE'])
+        results_per_time = results_per_time[["group", "time_step", "RMSE"]]
+        results_per_time = (
+            results_per_time.groupby(["group", "time_step"])["RMSE"].mean().reset_index()
+        )
+        results_per_time["RMSE"] = np.sqrt(results_per_time["RMSE"])
         results_per_cat = results.rename(columns={"MSE": "RMSE"})
-        results_per_cat = results_per_cat[['group', 'category', 'RMSE']]
-        results_per_cat = results_per_cat.groupby(['group', "category"])['RMSE'].mean().reset_index()
-        results_per_cat['RMSE'] = np.sqrt(results_per_cat['RMSE'])
+        results_per_cat = results_per_cat[["group", "category", "RMSE"]]
+        results_per_cat = (
+            results_per_cat.groupby(["group", "category"])["RMSE"].mean().reset_index()
+        )
+        results_per_cat["RMSE"] = np.sqrt(results_per_cat["RMSE"])
         metric = "RMSE"
     else:
         results_per_time = results
@@ -168,21 +205,27 @@ if __name__ == "__main__":
     # Set the legend box to fancy and add a shadow
     plt.rcParams["legend.fancybox"] = False
     plt.rcParams["legend.shadow"] = True
+
     def place_legend(ax):
-        ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.23),
-                  ncol=len(run_ids))
+        ax.legend(loc="upper left", bbox_to_anchor=(1.04, 1), ncol=1)
+
     # Create a list of markers and colors for the plots. Each run will be represented
     # by a the same marker and color across all plots.
-    names = [configs[run_id]['experiment']['name'] for run_id in run_ids]
     markers = matplotlib_markers(len(run_ids))
-    markers = {n: marker for n, marker in zip(names, markers)}
-    cmap = plt.get_cmap('tab10', len(run_ids))
-    colors = {n: cmap(k) for k, n in enumerate(names)}
+    markers = {n: marker for n, marker in zip(display_names.values(), markers)}
+    cmap = plt.get_cmap("tab10", len(run_ids))
+    colors = {n: cmap(k) for k, n in enumerate(display_names.values())}
 
     # Plot the metric for each time step, grouped by group
     fig, ax = plt.subplots()
-    sns.boxplot(data=results_per_time, x="time_step", y=metric, hue="group", ax=ax,
-                palette=colors)
+    sns.boxplot(
+        data=results_per_time,
+        x="time_step",
+        y=metric,
+        hue="display_name",
+        ax=ax,
+        palette=colors,
+    )
     ax.set_title(f"{eval_task} - {metric} over lead time")
     ax.set_xlabel("Lead time (hours)")
     ax.set_ylabel(metric)
@@ -191,42 +234,80 @@ if __name__ == "__main__":
     ax.set_xticklabels([f"{t}h" for t in results_per_time["time_step"].unique()])
     # Put the legend below the plot
     place_legend(ax)
-    plt.savefig(results_dir / f"{eval_task}_{metric}_lead_time.png",
-                bbox_inches='tight')
+    plt.savefig(results_dir / f"{eval_task}_{metric}_lead_time.png", bbox_inches="tight")
     # Do the same but with a line plot
     fig, ax = plt.subplots()
-    sns.lineplot(data=results_per_time, x="time_step", y=metric, hue="group", ax=ax,
-                 palette=colors, markers=markers)
+    sns.lineplot(
+        data=results_per_time,
+        x="time_step",
+        y=metric,
+        hue="display_name",
+        style="display_name",
+        ax=ax,
+        palette=colors,
+        markers=markers,
+    )
     ax.set_title(f"{eval_task} - {metric} over lead time")
     ax.set_xlabel("Lead time (hours)")
-    ax.set_ylabel(f'{metric} - 95% CI')
+    ax.set_ylabel(f"{metric} - 95% CI")
     ax.set_xticks(results_per_time["time_step"].unique())
     ax.set_xticklabels([f"{t}h" for t in results_per_time["time_step"].unique()])
     place_legend(ax)
-    plt.savefig(results_dir / f"{eval_task}_{metric}_lead_time_line.png",
-                bbox_inches='tight')
+    plt.savefig(results_dir / f"{eval_task}_{metric}_lead_time_line.png", bbox_inches="tight")
 
     # Plot the metric for each SSHS category, grouped by group
     fig, ax = plt.subplots()
-    sns.boxplot(data=results_per_cat, x="category", y=metric, hue="group", ax=ax,
-                palette=colors)
+    sns.boxplot(
+        data=results_per_cat,
+        x="category",
+        y=metric,
+        hue="display_name",
+        ax=ax,
+        palette=colors,
+    )
     ax.set_title(f"{eval_task} - {metric} over SSHS category")
     ax.set_xlabel("SSHS category")
     ax.set_ylabel(metric)
     ax.set_xticks(range(-1, 6))
     ax.set_xticklabels(["TD", "TS", "C1", "C2", "C3", "C4", "C5"])
     place_legend(ax)
-    plt.savefig(results_dir / f"{eval_task}_{metric}_category.png",
-                bbox_inches='tight')
+    plt.savefig(results_dir / f"{eval_task}_{metric}_category.png", bbox_inches="tight")
     # Do the same but with a line plot
     fig, ax = plt.subplots()
-    sns.lineplot(data=results_per_cat, x="category", y=metric, hue="group", ax=ax,
-                 palette=colors, markers=markers)
+    sns.lineplot(
+        data=results_per_cat,
+        x="category",
+        y=metric,
+        hue="display_name",
+        style="display_name",
+        ax=ax,
+        palette=colors,
+        markers=markers,
+    )
     ax.set_title(f"{eval_task} - {metric} over SSHS category")
     ax.set_xlabel("SSHS category")
-    ax.set_ylabel(f'{metric} - 95% CI')
+    ax.set_ylabel(f"{metric} - 95% CI")
     ax.set_xticks(range(-1, 6))
     ax.set_xticklabels(["TD", "TS", "C1", "C2", "C3", "C4", "C5"])
     place_legend(ax)
-    plt.savefig(results_dir / f"{eval_task}_{metric}_category_line.png",
-                bbox_inches='tight')
+    plt.savefig(results_dir / f"{eval_task}_{metric}_category_line.png", bbox_inches="tight")
+
+    # Make a barplot of the number of samples per SSHS category
+    # First, isolate the results from a single run
+    results_single = results[results["run_id"] == run_ids[0]]
+    fig, ax = plt.subplots()
+    sns.countplot(data=results_single, x="category", ax=ax)
+    ax.set_title("Number of samples per SSHS category - test set")
+    ax.set_xlabel("SSHS category")
+    ax.set_ylabel("Number of samples")
+    ax.set_xticks(range(7))
+    ax.set_xticklabels(["TD", "TS", "C1", "C2", "C3", "C4", "C5"])
+    # Add the number of samples above each bar
+    for p in ax.patches:
+        ax.annotate(
+            f"{p.get_height()/1000:.1f}k",
+            (p.get_x() + p.get_width() / 2, p.get_height()),
+            ha="center",
+            va="bottom",
+        )
+    plt.savefig(results_dir / "samples_per_category.png", bbox_inches="tight")
